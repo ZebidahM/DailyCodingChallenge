8d5b7e0c1df3c994979e2aba382f81565b53dd8b
ZebidahM <zebidahmanyara@gmail.com>
2025-03-07 23:49:32 +0300

    comments for JS file

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+/* script.js Real Estate Website Javascript
+
+This script manages interactivity for the website,
+specifically handling the contact form submission.
+*/
+
+// Wait for the DOM to fully load before executing scripts
 document.getElementById("contact-form").addEventListener("submit", function(event){
     event.preventDefault();
     alert("Thank you for your message! We will get back to you soon.");
